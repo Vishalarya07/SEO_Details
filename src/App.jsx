554ae36d097c0a85f97f88c5c45a3c150c717ef0
Vishalarya07 <aryav7196@gmail.com>
2024-03-01 02:50:17 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Routes from "./Routes";
 import { BrowserRouter as Router } from "react-router-dom";
+import ThreePage from "pages/Three";
 
 function App() {
   return (
     <Router>
-      <Routes />
+      <ThreePage/>
     </Router>
   );
 }
